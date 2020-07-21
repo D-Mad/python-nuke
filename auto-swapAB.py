e3408877d1605ed08d17a904f0aa04367c6167be
thanhdatit <thanh.datit1992@gmail.com>
2020-07-21 07:55:41 +0700

    Create auto-swapAB.py

---
+++ auto-swapAB.py
@@ -0,0 +1,18 @@
+
+names = []
+a =  nuke.selectedNodes()
+for s in a:
+    n = s['name'].value()
+    names.append(n)
+
+ 
+i = 0
+for i in range(0,len(a)):
+    p = nuke.toNode(names[i])
+    p.knob('selected').setValue(True)
+    nukescripts.swapAB(nuke.selectedNode())
+    p.knob('selected').setValue(False)
+    i = i+1
+    if i > len(a) :
+     break
+   
