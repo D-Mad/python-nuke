75aace8d2e28c81d5a29912e712c6ce8361d1971
thanhdatit <thanh.datit1992@gmail.com>
2021-03-23 00:04:38 +0700

    Add files via upload

--- Utilities.py
+++ Utilities.py
@@ -1,3 +1,21 @@
+"""
+ #How to install from NukeShare (nukepedia)
+
+import Utilities
+
+nuke.menu('Nuke').addCommand('Utilities/auto update', "Utilities.autoUpdateRead()")
+nuke.menu('Nuke').addCommand('Utilities/Read/auto reload', "Utilities.autoReload()")
+nuke.menu('Nuke').addCommand('Utilities/Selected/auto update', "Utilities.autoUpdateSelected()")
+nuke.menu('Nuke').addCommand('Utilities/SwapAB/SwapAB', "Utilities.autoSwapAB()")
+nuke.menu('Nuke').addCommand('Utilities/AutoMerge/AutoMerge', "Utilities.autoMerge()")
+nuke.menu('Nuke').addCommand('Utilities/Select_allGizmo', "Utilities.select_all_gizmo()")
+nuke.menu('Nuke').addCommand('Utilities/RendermanyWrite', "Utilities.RendermanyWrite()")
+"""
+
+
+
+
+
 import nuke 
 import nukescripts 
 
@@ -73,40 +91,49 @@ def scaleNodes(scalex,scaley):
 
 
 def autoMerge():
-
     listcur = nuke.selectedNodes()
-    print(listcur)
+    swapped = True
+    while swapped: 
+      swapped = False
+      for i in range(len(listcur)-1):
+          if(int(listcur[i]['xpos'].value())) > (int(listcur[i+1]['xpos'].value())): 
+                listcur[i],listcur[i+1]  =  listcur[i+1],listcur[i]
+                swapped = True 
+ 
+
+    m = nuke.nodes.Merge2(inputs=(listcur[0],listcur[1]))
+    m.knob('operation').setValue('plus')
+    m.setXpos(int(listcur[1]['xpos'].value()))
+    m.setYpos(int(listcur[1]['ypos'].value())+200)
   
-    m = nuke.nodes.Merge(inputs=(listcur[0],listcur[1]))
     i =  2
-    t = range(2,len(listcur))
+    t = range(2,len(listcur)) 
+    for i in t: 
 
-    m.knob('selected').setValue(True)
-    nukescripts.swapAB(nuke.selectedNode())
-    for i in t:  
+       m = nuke.nodes.Merge2(inputs=(m,listcur[i]))
+       m.knob('operation').setValue('plus') 
+       m.setXpos(int(listcur[i]['xpos'].value()))
+       m.setYpos(int(listcur[1]['ypos'].value())+200)
     
+      
+       i = i+1
+       if i > len(listcur):
+        break ; 
+          
+
+   
+
+def select_all_gizmo():
+    for node in nuke.allNodes():
+        if  type(node) == nuke.Gizmo and node.Class() != "Cryptomatte" :
+             
+            node['selected'].setValue(True)
+   
+def RendermanyWrite():
+    s = nuke.selectedNodes()    
+    for i in s :
+        k = 0
+        nuke.execute(i,int(i.dependencies()[k]['first'].getValue()),int(i.dependencies()[k]['last'].getValue()))
+        k = k + 1
      
-        m = nuke.nodes.Merge(inputs=(m,listcur[i])) 
-        m.knob('selected').setValue(True)
-        nukescripts.swapAB(nuke.selectedNode())
-    
-    
-        i = i+1
-        if i > len(listcur):
-           break ; 
-    scaleNodes(1,2)        
-    
-    
-     
 
-"""
- #How to install from NukeShare (nukepedia)
-
-import Utilities
-
-nuke.menu('Nuke').addCommand('Utilities/auto update', "Utilities.autoUpdateRead()")
-nuke.menu('Nuke').addCommand('Utilities/Read/auto reload', "Utilities.autoReload()")
-nuke.menu('Nuke').addCommand('Utilities/Selected/auto update', "Utilities.autoUpdateSelected()")
-nuke.menu('Nuke').addCommand('Utilities/SwapAB/SwapAB', "Utilities.autoSwapAB()")
-nuke.menu('Nuke').addCommand('Utilities/AutoMerge/AutoMerge', "Utilities.autoMerge()")
-"""
